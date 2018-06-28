b2b1334db337c6fe026e2f13e4a9fc89057c8a6f
adam <adamac92@googlemail.com>
2018-06-28 19:41:30 +0100

    re submit

--- WishList/data/ApplicationDbContext.cs
+++ WishList/data/ApplicationDbContext.cs
@@ -4,11 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
-namespace WishList.data
+namespace WishList.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        ApplicationDbContext(DbContextOptions options) : base(options)
+        public ApplicationDbContext(DbContextOptions options) : base(options)
         {
             
         }
